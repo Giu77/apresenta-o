2adfbb2e1d93d32526d630d920f100580c204b8e
giulia <116807603+Giu77@users.noreply.github.com>
2024-09-03 23:03:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "giulia";
+const sobreNome = "arcanjo";
+const idade = 17;
+const cidadeNatal = "curitiba";
+const altura = 1.56;
+
+console.log("O meu primeiro nome é: ");
+console.log(primeiroNome);
+console.log("O meu sobrenome é: ");
+console.log(sobreNome);
+console.log("A minha idade é: ");
+console.log(idade);
+console.log("Eu nasci em uma cidade chamada: ");
+console.log(cidadeNatal);
+console.log("minha altura é: ");
+console.log(altura);
